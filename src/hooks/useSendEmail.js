89512b2f910fa0cf7c517adadc2294b89adc00af
Mohit Paudyal <mohit.paudyal@gmail.com>
2025-10-02 07:02:19 -0500

    add proxy function

--- src/hooks/useSendEmail.js
+++ src/hooks/useSendEmail.js
@@ -79,13 +79,24 @@ const useSendEmail = () => {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_SITE_URL}/api/0001_send_email_fn`,
+        "/.netlify/functions/proxy",
+        "0001_send_email_fn",
+        "GET",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ to, subject, text, html }),
+          body: JSON.stringify({ to: tenantData.email, subject, text }),
         },
       );
+
+      // const response = await fetch(
+      //   `${import.meta.env.VITE_SITE_URL}/api/0001_send_email_fn`,
+      //   {
+      //     method: "POST",
+      //     headers: { "Content-Type": "application/json" },
+      //     body: JSON.stringify({ to, subject, text, html }),
+      //   },
+      // );
 
       const data = await response.json();
 
