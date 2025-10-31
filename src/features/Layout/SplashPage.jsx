f7544396c67a60d69b45d11361bc2bcf44000d4c
Mohit Paudyal <ak47@Mohits-MacBook-Pro.attlocal.net>
2025-10-31 12:09:54 -0500

    Updated Splash page
    - Updated splash page with ability to click and navigate to respective apps. Rental App
    however needs login priv and hence if the client logs in via the tile aka the Rental App Card in the landing page, they are logged in as a property owner automatically. To access their rental account, the client can login via the button on the top.
    
    We also decided that we should keep the button to allow property managers to login in from the top also present since we do need clearer distinction between them two.
    
    Furthermore, this is a design issue that probably needs addressing. The problem here is we don't really care if you are a tenant or owner, we should display stuffs relative to what you have. And we do that to some degree. Removing roles fully without understanding the scope might be difficult to address. Lets keep updating this and fix it as we go.

--- src/features/Layout/SplashPage.jsx
+++ src/features/Layout/SplashPage.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 
 import { HomeRounded, ReceiptRounded } from "@mui/icons-material";
-import { Box, Container, Stack, Typography } from "@mui/material";
-import { InvoiceDashboardRouteUri, PropertiesRouteUri } from "common/utils";
+import { Alert, Box, Container, Stack, Typography } from "@mui/material";
+import {
+  InvoiceDashboardRouteUri,
+  OwnerRole,
+  PropertiesRouteUri,
+} from "common/utils";
+import { useAuthenticateMutation } from "features/Api/firebaseUserApi";
 import TitleCard from "features/Layout/components/TitleCard/TitleCard";
 import { useAppTitle } from "hooks/useAppTitle";
 
 export default function SplashPage() {
   useAppTitle("Home");
   const navigate = useNavigate();
+
+  const [
+    authenticate,
+    {
+      isSuccess: isAuthSuccess,
+      isLoading: isAuthLoading,
+      isError: isAuthError,
+      error: authError,
+    },
+  ] = useAuthenticateMutation();
+
+  const handleAuth = () => {
+    authenticate(OwnerRole);
+  };
+
+  useEffect(() => {
+    if (!isAuthLoading && isAuthSuccess) {
+      window.location.replace(PropertiesRouteUri);
+    }
+  }, [isAuthLoading]);
+
+  if (isAuthError) {
+    return (
+      <Alert severity="error">
+        <Stack>
+          <Typography>Error during log in. Please try again later.</Typography>
+          <Typography variant="caption">{authError?.message}</Typography>
+        </Stack>
+      </Alert>
+    );
+  }
 
   return (
     <Box
@@ -68,7 +104,7 @@ export default function SplashPage() {
                 sx={{ fontSize: 32, color: "primary.main", mr: 1.5 }}
               />
             }
-            onClick={() => navigate(PropertiesRouteUri)}
+            onClick={handleAuth}
           />
           <TitleCard
             title="Invoicer App"
