269e1fa384cf22dffe4ddd98ea4d13b351c849f9
Mohit <44540219+mohit2530@users.noreply.github.com>
2026-06-27 08:17:05 -0600

    Added tests for next payment due date (#402)
    * Updated useGetSelectedPropertyDetails hook
    
    - Resolved issue where the date was not correct, when the current month was paid, since we were
    calculating the date wrong. Also added test for this feature
    
    - Updated nextRentalPaymentDate when current month is paid, so that the calculation would be from the
    last time the rent was paid, not the start of the lease term. The bug was that the calculation was
    hapenning from the start of the lease term. Also added tests for this.
    
    * add unit test
    
    ---------
    
    Co-authored-by: Mohit Paudyal <ak47@Mohits-MacBook-Pro.attlocal.net>

--- src/features/Rent/hooks/useVerifySubscriptionForProperties.js
+++ src/features/Rent/hooks/useVerifySubscriptionForProperties.js
@@ -1,14 +1,7 @@
-// AllowedPropertiesSubscription ...
 import dayjs from "dayjs";
 
 import { Role } from "features/Auth/AuthHelper";
-
-// defines the allowed properties for each of the associated plan
-const AllowedPropertiesSubscription = {
-  prod_ULLSTCXPNbCB1r: 2, // starter plan
-  prod_ULLVYsSQ3f2VnB: 10, // professional plan
-  prod_ULLWJBO7h4uOoG: 10000, // enterprise plan
-};
+import { DefaultRentXPropertiesLimit } from "features/Rent/constants";
 
 // useVerifySubscriptionForProperties ...
 // defines a function that verifies the subscription details for the logged in user.
@@ -32,7 +25,7 @@ export const useVerifySubscriptionForProperties = (
   if (userRole === Role.Owner) {
     const selectedSubscription = subscriptionOptions?.find(
       (option) =>
-        option.productId === latestSubscription?.subscriptionProductId,
+        option.productName === latestSubscription?.subscriptionProductName,
     );
 
     // check if the user is within the trial period
@@ -46,7 +39,7 @@ export const useVerifySubscriptionForProperties = (
     }
 
     const allowedPropertiesLimit =
-      AllowedPropertiesSubscription[selectedSubscription?.productId] || 0;
+      DefaultRentXPropertiesLimit[selectedSubscription?.productName] || 0;
 
     return {
       canAddProperty: activePropertiesCount < allowedPropertiesLimit,
